7193bfe4e264ded582e427792b3bac80756ff354
Tran Thai Tan <tantan992@gmail.com>
2019-11-26 17:15:11 +0700

    no message

--- Bloomr/BloomrTests/BloomrTests.swift
+++ Bloomr/BloomrTests/BloomrTests.swift
@@ -7,20 +7,27 @@
 //
 
 import XCTest
+@testable import Bloomr
 
 class BloomrTests: XCTestCase {
-
+    var mainContest: Contest!
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.mainContest = Contest(json: [:])
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.mainContest = nil
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testContest() {
+        self.mainContest.category = 1
+        self.mainContest.itemNumbers = 3
+        XCTAssertEqual(self.mainContest.contestType, ContestItemType.country)
+        XCTAssertEqual(self.mainContest.itemNumbers, 3)
+        XCTAssertEqual(self.mainContest.contestFormat, ContestItemFormatType.audio)
+        
     }
 
     func testPerformanceExample() {
